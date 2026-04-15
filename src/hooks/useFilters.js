019d4b65bacff1ae88e6be229fb89c422c047a48
PapaRamsey <miri.cud@gmail.com>
2026-04-15 20:36:16 +0200

    refactor(order-filters): add order filter constants and refactor filtering logic

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -1,34 +1,63 @@
 import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+import {
+  CUSTOMER_DEFAULT_ORDER_FILTERS,
+  EMPLOYEE_DEFAULT_ORDER_FILTERS,
+  ORDER_FILTER_COUNT_EXCLUDED_KEYS,
+  ORDER_FILTER_KEYS
+} from '../consts/order';
 import { useAuth } from './useAuth';
+
+const buildFiltersKey = (searchParams) => {
+  const params = new URLSearchParams();
+
+  // Using the allow list pattern to correctly manage the query
+  // params that are not filter-related (offset, search, sort, etc.)
+  ORDER_FILTER_KEYS.forEach((key) => {
+    searchParams
+      .getAll(key)
+      .forEach((value) => params.append(key, value));
+  });
+
+  params.sort();
+
+  return params.toString();
+};
+
+const parseFiltersFromKey = (filtersKey) => {
+  const params = new URLSearchParams(filtersKey);
+  const uniqueKeys = [...new Set(params.keys())];
+
+  return Object.fromEntries(
+    uniqueKeys.map((key) => {
+      const values = params.getAll(key);
+      return [key, values.length === 1 ? values[0] : values];
+    }),
+  );
+};
 
 export const useFilters = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const { isEmployee } = useAuth();
 
+  const filtersKey = React.useMemo(
+    () => buildFiltersKey(searchParams),
+    [searchParams],
+  );
+
   const filters = React.useMemo(() => {
-    const filterKeys = Object.keys(Object.fromEntries([...searchParams]));
+    const defaultFilters = isEmployee ? EMPLOYEE_DEFAULT_ORDER_FILTERS : CUSTOMER_DEFAULT_ORDER_FILTERS;
 
-    const defaultFilters = isEmployee
-      ? {}
-      : { unassigned: true, unscheduled: true };
+    return { ...defaultFilters, ...parseFiltersFromKey(filtersKey) };
+  }, [isEmployee, filtersKey]);
 
-    return filterKeys.reduce((acc, key) => {
-      const values = searchParams.getAll(key);
-
-      return {
-        ...acc,
-        [key]: values.length === 1 ? values[0] : values,
-      };
-    }, defaultFilters);
-  }, [isEmployee, searchParams]);
-
-  const filtersCount = React.useMemo(() => (
-    Object.keys(filters)
-      .filter((key) => !['endDate', 'unassigned', 'unscheduled'].includes(key))
-      .reduce((acc) => acc + 1, 0)
-  ), [filters]);
+  const filtersCount = React.useMemo(
+    () => Object.keys(filters)
+      .filter((key) => !ORDER_FILTER_COUNT_EXCLUDED_KEYS.includes(key))
+      .length,
+    [filters],
+  );
 
   const resetFilters = React.useCallback(() => {
     setSearchParams([]);
